55df4ebe56f6dc2c4099bc57bf59466eb661391d
marcinretecki <info@marcinretecki.com>
2016-08-24 21:46:37 +0200

    Added new functions

--- functions.php
+++ functions.php
@@ -53,14 +53,42 @@ $source_url = las_get_source_url();
 
 
 
-
 //
 // Dev redirect
+// it is actually redirecting all not logged in users, so it can be used for restricting content
 //
-if ( ( $_SERVER['REQUEST_URI'] !== '/' ) && !las_admin_dev_mode() ) {
-  header('Location: http://las.nocnasowa.pl/');
-  exit;
+function las_dev_redirect() {
+  if ( ( $_SERVER['REQUEST_URI'] !== '/' ) && !is_user_logged_in() ) {
+    header('Location: http://las.nocnasowa.pl/');
+    exit;
+  }
 }
+add_action('init', 'las_dev_redirect');
+
+
+//
+// Redirect non-editors or non-admins from admin area
+//
+function las_login_redirect(){
+  if ( !current_user_can( 'edit_posts' ) ) {
+    wp_redirect( 'http://las.nocnasowa.pl/' );
+    exit;
+  }
+}
+add_action( 'admin_init', 'las_login_redirect' );
+
+
+
+//
+// Rewrite author's page base
+//
+function las_rewrite_init() {
+  global $wp_rewrite;
+  $wp_rewrite->author_base = 'u';
+  $wp_rewrite->author_structure = '/' . $wp_rewrite->author_base . '/%author%';
+}
+add_action('init', 'las_rewrite_init');
+
 
 //
 // Add categories to pages
@@ -69,3 +97,68 @@ function las_add_taxonomies_to_pages() {
  register_taxonomy_for_object_type( 'category', 'page' );
  }
 add_action( 'init', 'las_add_taxonomies_to_pages' );
+
+
+//
+// Usuń Title z automatycznie generowanych linków
+//
+function remove_title_attributes( $input ) {
+  return preg_replace('/\s*title\s*=\s*(["\']).*?\1/', '', $input);
+};
+add_filter( 'wp_list_pages', 'remove_title_attributes' );
+add_filter( 'wp_list_categories', 'remove_title_attributes' );
+
+
+//
+// Usuń Admin Bar
+//
+add_filter('show_admin_bar', '__return_false');
+
+
+
+//
+// Remove archive pages
+//
+add_action('template_redirect', 'remove_archives');
+function remove_archives() {
+  global $wp_query, $post;
+
+  if ( is_day() || is_month() || is_year() || is_tag() ) {
+    $wp_query->set_404();
+  }
+
+  if ( is_feed() ) {
+    $day    = get_query_var('day');
+    $month  = get_query_var('month');
+    $year   = get_query_var('year');
+
+    if ( !empty( $day ) || !empty( $month ) || !empty( $year ) ) {
+      $wp_query->set_404();
+      $wp_query->is_feed = false;
+    }
+  }
+}
+
+
+
+//
+// Add new roles
+//
+function las_add_roles() {
+  add_role(
+      'basic_user',
+      __( 'Basic User' ),
+      array(
+          'read' => true,
+      )
+  );
+
+  add_role(
+      'advanced_user',
+      __( 'Advanced User' ),
+      array(
+          'read' => true,
+      )
+  );
+}
+//las_add_roles(); // we do it only once!
--- kursy.php
+++ kursy.php
@@ -15,193 +15,51 @@ include( 'includes/head.php' );
 
   <h2 class="h1 size-4">Kursy</h2>
 
-  <h2 class="h1 size-4">Podstawy, bez których nie powinieneś iść dalej</h2>
+  <?php
+      $current_user = wp_get_current_user();
 
-  <ol>
-    <li>Jak znaleźć drogę w Lesie?</li>
-    <li>Sztuka robienia notatek</li>
-    <li>Metoda <i>deliberate practice</i></li>
-
-    <li>
-      Czy umiesz odmieniać rzeczownik?
-      <ul>
-        <li>Czy umiesz znaleźć rodzajnik i odmianę rzeczownika w słowniku?</li>
-        <li>Czy uczysz się rzeczowników razem z rodzajnikiem?</li>
-        <li>Czy rozróżniasz formę określoną od nieokreślonej?</li>
-        <li>Czy umiesz tworzyć liczbę mnogą w formie nieokreślonej i określonej?</li>
-        <li>Czy znasz liczby po norwesku?</li>
-        <li>Czy znasz podstawowe wyjątki w odmianie rzeczownika?</li>
-        <li>Czy umiesz używać zaimków osobowych, żeby zastąpić rzeczownik lub imię?</li>
-      </ul>
-    </li>
-
-    <li>
-      Czy umiesz tworzyć czas teraźniejszy?
-      <ul>
-        <li>Czy wiesz, jaką końcówkę dodajemy do czasownika? (powtarzanie na głos)</li>
-        <li>Czy wiesz, kiedy używamy znaku bezokolicznika <em>å</em>? (powtarzanie na głos)</li>
-        <li>Czy wiesz, co się dzieje z drugim/trzecim/czwartym czasownikiem w zdaniu? (powtarzanie na głos)</li>
-        <li>Czy umiesz odmieniać często używane czasowniki nieregularne? (znasz czasowniki, które odmieniają się nieregularnie?)</li>
-        <li>Czy umiesz używać 4 najpopularniejszych czasowników modalnych?</li>
-        <li>Czy umiesz używać czasu teraźniejszego, żeby wyrazić przyszłość?</li>
-        <li>Czy znasz określenia czasu w czasie teraźniejszym i przyszłym? (dni tygodnia, miesiące, lata, itp)</li>
-      </ul>
-    </li>
-
-    <li>Czy umiesz układać zdania zgodnie z szykim prostym?
-
-      <ul>
-        <li>Czy pamiętasz o tym, żeby w każdym zdaniu był podmiot (osoba) i orzeczenie (odmieniony czasownik)?</li>
-        <li>Czy pamiętasz, na którym miejscu zdania jest orzeczenie?</li>
-        <li>Czy pamiętasz, gdzie wstawiamy <em>ikke</em>?</li>
-        <li>Czy pamiętasz, w jakiej kolejności są okoliczniki (miesca, czasu, sposobu) w zdaniu?</li>
-        <li>Czy umiesz zadawać pytania przez “czy”?</li>
-        <li>Czy umiesz zadawać pytania z zaimkami pytającymi (<em>hva, hvem, hvor, når</em> itd.)</li>
-      </ul>
-    </li>
+      echo '<pre style="display:none">';
+      print_r( $current_user );
+      echo '</pre>';
 
 
-    <li>
-      Czy umiesz opowiedzieć o sobie?
+    function las_show_courses() {
 
-      <ul>
-        <li>Czy umiesz powiedzieć skąd pochodzisz, gdzie mieszkasz i jak się nazywasz?</li>
-        <li>Czy umiesz opowiedzieć o swojej najbliższej rodzinie?</li>
-        <li>Czy umiesz opowiedzieć, czym zajmujesz się w pracy, w czym jesteś wykwalifikowany?</li>
-        <li>Czy umiesz opowiedzieć o swoich zainteresowaniach, swoim hobby albo sporcie, który trenujesz?</li>
-        <li>Czy umiesz porozumieć się w sklepie, zamówić jedzenie w restauracji, zapytać o cenę?</li>
-        <li>Czy umiesz</li>
-      </ul>
-    </li>
+      if ( current_user_can( 'edit_posts' ) || current_user_can( 'avanced_user' ) ) {
+        // if it is an editor or advanced user, we can show him advanced course
+        // later it would be wise to add other courses here as well
+        $post_parent__in = array(20, 24);
+      }
+      else if ( current_user_can( 'basic_user' ) ) {
+        // if it is a basic user, show just basic course
+        $post_parent__in = array(20);
+      }
 
+      $course_args = array(
+        'post_type'       => 'page',
+        'post_parent__in' => $post_parent__in,
+        'posts_per_page'  => -1,
+        'orderby'         => 'menu_order',
+        'nopaging'        => true,
+        'order'           => 'ASC'
+      );
 
-    <li>
-      Czy umiesz wymawiać samogłoski?
+      $courses = get_posts( $course_args );
 
-      <ul>
-        <li>Czy umiesz ułożyć odpowiednio usta, gdy wymawiasz samogłoski?</li>
-        <li>Czy rozróżniasz wymowę <i>ø</i> od <i>å</i>? (wycinki filmów i nagrań)</li>
-        <li>Czy rozróżniasz wymowę <i>u</i> od <i>o</i>?</li>
-        <li>Czy rozróżniasz wymowę <i>æ</i> od <i>a</i>?</li>
-        <li>Czy rozróżniasz wymowę <i>i</i> od <i>y</i>?</li>
-        <li>Czy rozróżniasz wymowę <i>e</i> od <i>ø</i>?</li>
-        <li>Czy umiesz przeliterować swoje imię i nazwisko?</li>
-        <li>Czy umiesz poprawnie wymówić swój adres zamieszkania?</li>
-        <li>Czy umiesz wymówić nazwy miast lub dzielnic, po których się poruszasz?</li>
-      </ul>
-    </li>
-  </ol>
+      $current_level = 'basic';
 
-  <h2 class="h1 size-4">Dalsze zagadnienia</h2>
+      foreach ( $courses as $post ) : setup_postdata( $post );
 
-  <ol>
-    <li>
-      Czy umiesz tworzyć czas przeszły (<em>preteritum</em>)?
+        print_r( get_the_category() );
 
-      <ul>
-        <li>Czy umiesz znaleźć odmianę czasownika w słowniku?</li>
-        <li>Czy wiesz, którą formę czasownika użyć w czasie przeszłym?</li>
-        <li>Czy wiesz, co się dzieje z drugim/trzecim/czwartym czasownikiem w zdaniu, gdy pierwszy jest w czasie przeszłym?</li>
-        <li>Czy umiesz opisać swój wczorajszy dzień?</li>
-        <li>Czy umiesz powiedzieć, że coś zdarzyło się wczoraj, tydzień temu, w zeszłym roku, 5 lat temu?</li>
-        <li>Czy umiesz powiedzieć, jak długo coś robiłeś (np. studiowałeś, pracowałeś) w przeszłości?</li>
-        <li>Czy umiesz zapytać kolegę, jak spędził wakacje?</li>
-      </ul>
-    </li>
+      endforeach;
+      wp_reset_postdata();
+
+    }
+    las_show_courses();
 
 
-    <li>
-      Czy umiesz używać konstrukcji <em>presens perfektum</em>?
-
-      <ul>
-        <li>Czy umiesz stworzyć <em>presens perfektum</em>?</li>
-        <li>Czy wiesz w jakich sytuacjach używamy <em>presens perfektum</em>?</li>
-        <li>Czy rozróżniasz <em>presens perfektum</em> od <em>preteritum</em>?</li>
-        <li>Czy wiesz, którą formę czasownika użyć w <em>presens perfektum</em>?</li>
-        <li>Czy znasz określenia czasu używane w <em>presens perfektum</em>?</li>
-        <li>Czy umiesz używać <em>i</em> w <em>presens perfektum</em>?</li>
-        <li>Czy umiesz używać <em>siden</em> w <em>presens perfektum</em>?</li>
-        <li>Czy umiesz powiedzieć od jak dawna mieszkach w Norwegii albo od jak dawna jesteś w swojej obecnej pracy?</li>
-        <li>Czy umiesz podkreślić skutki zdarzeń z przeszłości bez określania, kiedy miały miejsce?</li>
-      </ul>
-    </li>
-
-
-    <li>
-      Czy umiesz używać używać konstrukcji <em>preteritum perfektum</em> i strony biernej?
-
-      <ul>
-        <li>Czy umiesz używać <em>perfektum partisipp</em> razem z <em>å være</em>? (Maten er/var spist)</li>
-        <li>Czy umiesz używać <em>perfektum partisipp</em> razem z <em>å bli</em>? (Maten blir/ble spist.)</li>
-        <li>Czy umiesz używać czasowników w formie <em>perfektum partisipp</em> w funkcji przydawki (np. <strong>spóźniony</strong> pociąg, <strong>spóźnione</strong> pociągi)?</li>
-        <li>Czy umiesz używać czasowników w formie <em>perfektum partisipp</em> w funkcji orzecznika (np. pociąg jest <strong>spóźniony</strong>, pociągi są <strong>spóźnione</strong>)?</li>
-        <li>Czy umiesz stworzyć czas <em>preteritum perfektum</em>?</li>
-        <li>Czy rozróżniasz czas <em>preteritum perfektum</em> od <em>preteritum</em>?</li>
-        <li>Czy umiesz używać formy <em>perfektum partisipp</em> razem z czasownikami modalnymi?</li>
-      </ul>
-    </li>
-
-
-    <li>
-      Czy umiesz zmieniać szyk zdania prostego?
-
-      <ul>
-        <li>Czy znasz pełny szyk norweskiego zdania (7 podstawowych części zdania)?</li>
-        <li>Czy umiesz przestawiać okoliczniki (sposobu, miejsca, czasu, przyczyny) na pierwszą pozycję zdania?</li>
-        <li>Czy pamiętasz o pozycji orzeczenia i podmiotu po zmianie szyku zdania?</li>
-        <li>Czy znasz najważniejsze okoliczniki zdaniowe?</li>
-        <li>Czy znasz spójniki współrzędne (po których nie zmieniamy szyku)?</li>
-      </ul>
-    </li>
-
-
-    <li>
-      Czy umiesz budować zdania podrzędnie złożone? (znasz szyk podrzędny?)
-
-      <ul>
-        <li>Czy pamiętasz, które spójniki wprowadzają zdanie podrzędne?</li>
-        <li>Czy pamiętasz, na której pozycji zdania podrzędnego jest podmiot?</li>
-        <li>Czy pamiętasz, co dzieje się z okolicznikiem zdaniowym w zdaniu podrzędnym?</li>
-        <li>Czy umiesz przestawić zdanie podrzędne na pierwszą pozycję zdania nadrzędnego?</li>
-        <li>Czy umiesz stworzyć mowę zależną?</li>
-        <li>Czy pamiętasz, co się dzieje w zdaniu podrzędnym, gdy zdanie nadrzędne jest w czasie przeszłym?</li>
-        <li>Czy rozróżniasz spójniki podrzędne od okoliczników zdaniowych?</li>
-      </ul>
-    </li>
-
-
-    <li>
-      Czy umiesz odmieniać przymiotniki?
-
-      <ul>
-        <li>Czy umiesz odmieniać przymiotnik zgodnie z rodzajem rzeczownika (forma nieokreślona)?</li>
-        <li>Czy umiesz odmieniać przymiotnik zgodnie z liczbą rzeczownika (forma nieokreślona)?</li>
-        <li>Czy umiesz używać przymiotników, gdy rzeczownik jest w formie określonej?</li>
-        <li>Czy umiesz używać przymiotników przy zaimkach dzierżawczych (<em>min, mitt, mine</em> itd)?</li>
-        <li>Czy umiesz używać przymiotników przy zaimkach wskazujących i rodzajnikach określonych (<em>den, det, de</em> itd)?</li>
-      </ul>
-    </li>
-
-
-    <li>
-      Czy umiesz określić przynależność?
-
-      <ul>
-        <li>Czy umiesz używać zaimków dzierżawczych?</li>
-        <li>Czy umiesz używać <em>genitiv -s</em>?</li>
-      </ul>
-    </li>
-
-
-    <li>
-      Czy umiesz określić położenie?
-
-      <ul>
-        <li>Czy znasz przyimki określające położenie?</li>
-        <li>Czy znasz przysłówki określające położenie?</li>
-      </ul>
-    </li>
-  </ol>
+  ?>
 
 </section>
 
