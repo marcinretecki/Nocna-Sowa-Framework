84fd844737e9bbec8b6ec3323bfd92b4ff3e14e1
marcinretecki <info@marcinretecki.com>
2017-07-09 17:00:49 +0200

    Przewodniki z liczbami

---
+++ data/przewodnik/liczby-0.php
@@ -0,0 +1,152 @@
+<?php
+//
+//  Przewodnik - Liczby 0
+//
+
+?>
+
+
+<p>Ćwiczenia na liczby rozbiliśmy na trzy zakresy:</p>
+
+<ul>
+<li>od 0 do 19</li>
+<li>od 20 do 99</li>
+<li>i powyżej 100</li>
+</ul>
+
+<h3>Zacznijmy od cyfr.</h3>
+
+<p>Przy każdej liczbie zapisaliśmy, co się dzieje z jej wymową. Czasem nie będziemy wymawiali jakiejś litery, a czasem dwie litery połączą się w jeden dźwięk. Obok każdej cyfry jest nagranie. Nie wystarczy, że go posłuchasz, musisz samemu przećwiczyć wymowę na głos.</p>
+
+
+<div class="przewodnik-table-wrapper">
+  <table class="przewodnik-table">
+    <tbody>
+      <tr>
+        <td>0</td>
+        <td><i class="no-break">null</i></td>
+        <td>Wymawiamy krótko, przez miękkie <i class="mark mark--table">u</i>.</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td><i class="no-break">én, éi, ett</i></td>
+        <td>Liczba jeden odmienia się zgodnie z rodzajem. Zauważ, że jest bardzo podobna do rodzajników <i class="mark mark--table">en</i>, <i class="mark mark--table">ei</i>, <i class="mark mark--table">et</i>. Kreseczka w znaku <i class="mark mark--table">é</i> to akcent. Wszystkie wymawiamy z uśmiechem.</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td><i class="no-break">to</i></td>
+        <td>Czytamy przez polskie [ó].</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td><i class="no-break">tre</i></td>
+        <td>Wymawiamy z uśmiechem.</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td><i class="no-break">fire</i></td>
+        <td>Norweskie <i class="mark mark--table">i</i> też jest szeroko uśmiechnięte.</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td><i class="no-break">fem</i></td>
+        <td>Wymawiamy krótko.</td>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td><i class="no-break">seks</i></td>
+        <td>Wszystkim się trochę kojarzy, dla Norwegów to nic nadzwyczajnego.</td>
+      </tr>
+      <tr>
+        <td>7</td>
+        <td><i class="no-break">sju</i></td>
+        <td>Wymawiamy przez polskie [sz], a później miękkie <i class="mark mark--table">u</i>. Z tą cyfrą wiele osób ma problem, nie poddawaj się, ćwicz wiele razy.</td>
+      </tr>
+      <tr>
+        <td>8</td>
+        <td><i class="no-break">åtte</i></td>
+        <td><i class="mark mark--table">Å</i> czytamy podobnie do polskiego [o], ale głębiej. Zacinamy się też na podwójnym <i class="mark mark--table">t</i>.</td>
+      </tr>
+      <tr>
+        <td>9</td>
+        <td><i class="no-break">ni</i></td>
+        <td>Wymawiamy z uśmiechem.</td>
+      </tr>
+
+    </tbody>
+  </table>
+</div>
+
+<p>Jeśli pierwszy raz uczysz się liczb, przećwicz najpierw te powyżej, zanim przejdziesz do dziesiątek.</p>
+
+<h3>Od 10 do 19</h3>
+
+<div class="aid-table-wrapper">
+  <table class="aid-table aid-table-np">
+    <tbody>
+      <tr><td>100   </td>   <td>(et) hundre    </td>   <td>wymawiamy przez miękkie u    </td></tr>
+    </tbody>
+  </table>
+</div>
+
+
+<div class="przewodnik-table-wrapper">
+  <table class="przewodnik-table">
+    <tbody>
+
+      <tr>
+        <td>10</td>
+        <td><i class="no-break">ti</i></td>
+        <td>Wymawiamy z uśmiechem.</td>
+      </tr>
+      <tr>
+        <td>11</td>
+        <td><i class="no-break">elleve</i></td>
+        <td>Tu uwaga, czytamy jakby było napisane [elve].</td>
+      </tr>
+      <tr>
+        <td>12</td>
+        <td><i class="no-break">tolv</i></td>
+        <td>Nie czytamy ostatniego [v].</td>
+      </tr>
+      <tr>
+        <td>13</td>
+        <td><i class="no-break">tretten</i></td>
+        <td>Końcówki <i class="mark mark--table">-en</i> możesz wymówić wyraźnie, lub je lekko zjeść. W nagraniu są obie wersje.</td>
+      </tr>
+      <tr>
+        <td>14</td>
+        <td><i class="no-break">fjorten</i></td>
+        <td>Czytamy przez polskie [ó].</td>
+      </tr>
+      <tr>
+        <td>15</td>
+        <td><i class="no-break">femten</i></td>
+        <td>Tu się nic nie dzieje! Uff :) </td>
+      </tr>
+      <tr>
+        <td>16</td>
+        <td><i class="no-break">seksten</i></td>
+        <td>Czytam jakby było napisane [sajsten].</td>
+      </tr>
+      <tr>
+        <td>17</td>
+        <td><i class="no-break">sytten</i></td>
+        <td>Wymawiamy przez [e], [setten]</td>
+      </tr>
+      <tr>
+        <td>18</td>
+        <td><i class="no-break">atten</i></td>
+        <td>Tu nie ma kółeczka nad <i class="mark mark--table">a</i>, więc głęboko, gardłowo.</td>
+      </tr>
+      <tr>
+        <td>19</td>
+        <td><i class="no-break">nitten</i></td>
+        <td>Z uśmiechem.</td>
+      </tr>
+
+    </tbody>
+  </table>
+</div>
+
+<p>W wyzwaniu znajdziesz ten sam zestaw liczb, ale w losowej kolejności. Możesz ćwiczyć je wiele razy, aż przestaną sprawiać Ci jakiekolwiek trudności. Zawsze też możesz wrócić do wyzwania, żeby powtórzyć wymowę.</p>
---
+++ data/przewodnik/liczby-20.php
@@ -0,0 +1,99 @@
+<?php
+//
+//  Przewodnik - Liczby 20
+//
+
+?>
+
+<p>Gdy już znasz jedności, możemy zacząć je łączyć z dziesiątkami. Zacznijmy od wymowy.</p>
+
+
+<div class="przewodnik-table-wrapper">
+  <table class="przewodnik-table">
+    <tbody>
+      <tr class="przewodnik-table__audio-row">
+        <td>20</td>
+        <td><i class="no-break">tjue</i></td>
+        <td>Wymawiamy przez bezdźwięczne [ś] (więcej o tym innym razem). Możesz na początku wymawiać polskie [ś], jakby było [siue].</td>
+      </tr>
+      <tr class="przewodnik-table__audio-row">
+        <td>30</td>
+        <td><i class="no-break">tretti</i></td>
+        <td>Wymawiamy z uśmiechem i zacinamy się na podwójnym <i class="mark">t</i>.</td>
+      </tr>
+      <tr class="przewodnik-table__audio-row">
+        <td>40</td>
+        <td><i class="no-break">førti</i></td>
+        <td>Pamiętaj o norweskim <i class="mark mark--table">ø</i>. Wymawiamy je podobnie jak polskie [e], ale robimy przy tym dzióbek. Nigdy nie wymawiaj <i class="mark mark--table">ø</i> jak polskiego [o] lub [u].</td>
+      </tr>
+      <tr class="przewodnik-table__audio-row">
+        <td>50</td>
+        <td><i class="no-break">femti</i></td>
+        <td>Krótko, zacinamy się na <i class="mark mark--table">m</i>.</td>
+      </tr>
+      <tr class="przewodnik-table__audio-row">
+        <td>60</td>
+        <td><i class="no-break">seksti</i></td>
+        <td>Zauważ, że prawie wszystkie dziesiątki kończą się na <i class="mark mark--table">-ti</i>. <i>Seks-ti</i> – sześć dziesiątek.</td>
+      </tr>
+      <tr class="przewodnik-table__audio-row">
+        <td>70</td>
+        <td><i class="no-break">sytti</i></td>
+        <td>Wymawiamy przez [e], jakby było [setti].</td>
+      </tr>
+      <tr class="przewodnik-table__audio-row">
+        <td>80</td>
+        <td><i class="no-break">åtti</i></td>
+        <td><i class="mark mark--table">Å</i> czytamy podobnie do polskiego [o], ale głębiej. Zacinamy się też na podwójnym <i class="mark mark--table">t</i>.</td>
+      </tr>
+      <tr class="przewodnik-table__audio-row">
+        <td>90</td>
+        <td><i class="no-break">nitti</i></td>
+        <td>Z uśmiechem i zacinamy się na <i class="mark mark--table">t</i>.</td>
+      </tr>
+</tbody>    </tbody>
+  </table>
+</div>
+
+<p>Jak więc robimy łączenia z jednościami? Piszemy je łącznie. Niczego nie trzeba dodawać, zwyczajnie wymawiasz obie liczby. </p>
+
+<div class="przewodnik-table-wrapper">
+  <table class="przewodnik-table">
+    <tbody>
+
+      <tr class="przewodnik-table__audio-row">
+        <td>38</td>
+        <td><i class="no-break">trettiåtte</i></td>
+      </tr>
+      <tr class="przewodnik-table__audio-row">
+        <td>42</td>
+        <td><i class="no-break">førtito</i></td>
+      </tr>
+
+    </tbody>
+  </table>
+</div>
+
+
+<p>20 często myli się z 7. Jak więc powiesz 27?</p>
+
+<div class="przewodnik-table-wrapper">
+  <table class="przewodnik-table">
+    <tbody>
+
+      <tr class="przewodnik-table__audio-row">
+        <td>27</td>
+        <td><i class="no-break">tjuesju</i></td>
+        <td>[siue-szu]</td>
+      </tr>
+      <tr class="przewodnik-table__audio-row">
+        <td>42</td>
+        <td><i class="no-break">førtito</i></td>
+        <td></td>
+      </tr>
+
+    </tbody>
+  </table>
+</div>
+
+<p>Dużo więcej przykładów będzie w wyzwaniu. Za każdym razem, gdy do niego wejdziesz, będzie tam na Ciebie czekał inny zestaw liczb do przećwiczenia.</p>
